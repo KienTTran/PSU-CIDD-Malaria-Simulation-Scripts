336d5250a1816ca7900f68b39daca3c180e1c1b0
KienTTran <ktt5121@psu.edu>
2022-05-23 10:05:16 -0400

    test

--- python/PRMC/calculate_LD_test.py
+++ python/PRMC/calculate_LD_test.py
@@ -55,7 +55,7 @@ try:
     csv_db.columns = ["id","aa_sequence"]   
     genotype_names = csv_db.aa_sequence
     genotype_names = pd.concat([genotype_names, pd.Series(["temp"])], ignore_index=True)
-    csv_freq = pd.read_csv(file_path_freq, sep='\t', header=None, names = genotype_names, index_col=None)
+    csv_freq = pd.read_csv(file_path_freq, sep='\t', header=None, names = genotype_names)
     csv_freq = csv_freq.fillna(0)
     csv_freq = csv_freq.drop(['temp'], axis=1)  
     data.append(csv_freq)
@@ -65,32 +65,43 @@ except Exception as e:
     
 data_plot_2 = pd.concat(data,ignore_index=True, axis = 0)    
 
-initial_genotypes = [
-                        "||||YY1||KTHFI,x||||||FNMYRIPRPC|1", 
-                        "||||YY1||TTHFI,x||||||FNMYRIPRPY|1"
-                     ]
-X_i = []
-mutated_allele_pairs = []
-mutated_loci = []
+# initial_genotypes = [
+#                         "||||YY1||KTHFI,x||||||FNMYRIPRPC|1", 
+#                         "||||YY1||TTHFI,x||||||FNMYRIPRPY|1"
+#                      ]
+# X_i = []
+# mutated_allele_pairs = []
+# mutated_loci = []
 
-data_columns = data_plot.columns
+# data_columns = data_plot.columns
             
-for index,pair in enumerate(zip(*data_columns)):
-    mutated_allele = []
-    for elem in pair:
-        if elem not in mutated_allele:
-            mutated_allele.append(elem)
-    if len(mutated_allele) > 1:
-        mutated_allele_pairs.append(mutated_allele)
-        mutated_loci.append(index)
+# for index,pair in enumerate(zip(*data_columns)):
+#     mutated_allele = []
+#     for elem in pair:
+#         if elem not in mutated_allele:
+#             mutated_allele.append(elem)
+#     if len(mutated_allele) > 1:
+#         mutated_allele_pairs.append(mutated_allele)
+#         mutated_loci.append(index)
         
-# Calculate Allele freq     
-for allele_pair in mutated_allele_pairs:
-    for allele in allele_pair:
-        data_plot["Allele " + allele] = 0
-        data_plot["X_" + allele] = 0
+# # Calculate Allele freq     
+# for allele_pair in mutated_allele_pairs:
+#     for allele in allele_pair:
+#         data_plot["Allele " + allele] = 0
+#         data_plot["X_" + allele] = 0
         
-for column in data_columns:
-    for index,pos in enumerate(mutated_loci):
-        if column[pos] == mutated_allele_pairs[index][1]:
-            data_plot["Allele " + column[pos]] += data_plot[column]
+# for column in data_columns:
+#     for index,pos in enumerate(mutated_loci):
+#         if column[pos] == mutated_allele_pairs[index][1]:
+#             data_plot["Allele " + column[pos]] += data_plot[column]
+            
+#%%
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+plt.close("all")   
+plot = sns.lineplot(data=data_plot_2, x=data_plot_2.index, y="LD") 
+plot.set_xlabel('Month')
+plot.set_ylabel('LD')
+# plot.set_yticks(range(0,100,10))
+# plot.set(xlim=(0, 10))
